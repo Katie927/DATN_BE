19fddb15cba0335169d5ed89207b760cf1a043f0
Tong Quang Nam <suongthu2003@gmail.com>
2025-11-10 19:08:24 +0700

    config rabbitmq for websocket

---
+++ src/main/java/com/DATN/Bej/config/WebSocketConfig.java
@@ -0,0 +1,37 @@
+package com.DATN.Bej.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
+    /**
+     * 
+     */
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+            .setAllowedOrigins("*")
+            .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+            registry.setApplicationDestinationPrefixes("/app");
+
+        registry.enableStompBrokerRelay("/topic", "/queue")
+                .setRelayHost("localhost")
+                .setRelayPort(61613)
+                .setClientLogin("guest")
+                .setClientPasscode("guest");
+                // .setSystemLogin("guest") 
+                // .setSystemPasscode("guest");
+
+        registry.setUserDestinationPrefix("/user");
+    }
+
+}
