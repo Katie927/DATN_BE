617119679faed5b56f8c0c75c31b9b32c1d1c2d8
Tong Quang Nam <suongthu2003@gmail.com>
2025-11-14 00:17:54 +0700

    update NotificaionEventListener

--- src/main/java/com/DATN/Bej/BejApplication.java
+++ src/main/java/com/DATN/Bej/BejApplication.java
@@ -2,8 +2,10 @@ package com.DATN.Bej;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class BejApplication {
 
 	public static void main(String[] args) {
